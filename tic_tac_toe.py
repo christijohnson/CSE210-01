38a3f3686fa40c1cff7061a88ef6dea2d3e34875
christijohnson <97490948+christijohnson@users.noreply.github.com>
2022-01-14 13:45:12 -0700

    enhanced game play

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,11 +10,13 @@ def main():
 
 def draw_board():
     global positions
+    print()
     print(f"{positions[0]} | {positions[1]} | {positions[2]}")
     print("---------")
     print(f"{positions[3]} | {positions[4]} | {positions[5]}")
     print("---------")
     print(f"{positions[6]} | {positions[7]} | {positions[8]}")
+    print()
 
 def play_game():
     global player
@@ -29,6 +31,19 @@ def play_game():
         positions[selection - 1] = player
         draw_board()
         turn_count += 1
+
+        if (positions[0] == positions[1] == positions[2]  or
+            positions[3] == positions[4] == positions[5]  or
+            positions[6] == positions[7] == positions[8]  or
+            positions[0] == positions[3] == positions[6]  or
+            positions[1] == positions[4] == positions[7]  or
+            positions[2] == positions[5] == positions[8]  or
+            positions[0] == positions[4] == positions[8]  or
+            positions[2] == positions[4] == positions[6]):
+            print(f"Good game!  Player {player} is the winner!! Thanks for playing Tic-Tac-Toe.")
+            turn_count = 9
+        elif turn_count == 9:
+            print("No winner, the game is a draw, you should play again!")
 
 if __name__ == "__main__":  
     main()
